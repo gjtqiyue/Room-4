852318a832144e598c385aa0c3c08b6c8fe6e3e0
gjtqiyue <gjtqiyue@gmail.com>
2017-10-29 16:29:49 -0400

    Simple setup
    Set up a simple menu and create all the folders we need so we can start adding things on it

---
+++ Room #4/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+	public AudioSource aud;
+	public Button startButton;
+	public Text startText;
+	public Text titleText;
+
+	Animator anim;
+
+	void Start () {
+		aud = GetComponent <AudioSource> ();
+		startButton = GetComponent <Button> ();
+		anim = GetComponent <Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void enterButton () {
+		aud.Play ();
+
+		anim.SetTrigger ("Start");
+
+	}
+
+}
